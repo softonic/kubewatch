17f0315839ac9dee852163c05dd2142ee775594b
Marc Villacorta <marc.villacorta@gmail.com>
2017-02-27 14:44:08 +0100

    Added --resource and --namespace flags

--- kubewatch.go
+++ kubewatch.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/fields"
+	"k8s.io/client-go/pkg/runtime"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -18,8 +19,16 @@ import (
 func main() {
 
 	// Handle flags:
-	kubeconfig := flag.String("kubeconfig", "./config", "absolute path to the kubeconfig file")
+	kubeconfig := flag.String("kubeconfig", "./config", "Absolute path to the kubeconfig file.")
+	resource := flag.String("resource", "services", "Set the resource type to be watched.")
+	namespace := flag.String("namespace", "default", "Set the namespace to be watched.")
 	flag.Parse()
+
+	// Map resource to runtime object:
+	m := map[string]runtime.Object{
+		"pods":     &v1.Pod{},
+		"services": &v1.Service{},
+	}
 
 	// Uses the current context in kubeconfig:
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -33,25 +42,25 @@ func main() {
 		panic(err.Error())
 	}
 
-	// Watch for services in the default namespace:
+	// Watch for resource in namespace:
 	watchlist := cache.NewListWatchFromClient(
 		clientset.Core().RESTClient(),
-		"services", v1.NamespaceDefault,
+		*resource, *namespace,
 		fields.Everything())
 
 	_, controller := cache.NewInformer(
 		watchlist,
-		&v1.Service{},
+		m[*resource],
 		time.Second*0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				fmt.Printf("service added: %s \n", obj)
+				fmt.Printf("%s added: %s \n", *resource, obj)
 			},
 			DeleteFunc: func(obj interface{}) {
-				fmt.Printf("service deleted: %s \n", obj)
+				fmt.Printf("%s deleted: %s \n", *resource, obj)
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
-				fmt.Printf("service changed \n")
+				fmt.Printf("%s changed \n", *resource)
 			},
 		},
 	)
